3b1245d6e3be03c658659d61d3cec0924e6323e8
Daniel Price <danielprice188@hotmail.com>
2020-05-19 14:09:50 +0100

    Add GetDown 0.1

---
+++ GetDown/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace GetDown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tslMostRecentStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUri = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbRetry = new System.Windows.Forms.ComboBox();
+            this.dgvStatus = new System.Windows.Forms.DataGridView();
+            this.chkPlaySound = new System.Windows.Forms.CheckBox();
+            this.btnExpand = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.chkUseProxy = new System.Windows.Forms.CheckBox();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStatus)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tslMostRecentStatus,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 419);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(460, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tslMostRecentStatus
+            // 
+            this.tslMostRecentStatus.Name = "tslMostRecentStatus";
+            this.tslMostRecentStatus.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel1.Text = "Ready";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URI to query:";
+            // 
+            // txtUri
+            // 
+            this.txtUri.Location = new System.Drawing.Point(12, 25);
+            this.txtUri.Name = "txtUri";
+            this.txtUri.Size = new System.Drawing.Size(354, 20);
+            this.txtUri.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Times to query:";
+            // 
+            // cmbRetry
+            // 
+            this.cmbRetry.FormattingEnabled = true;
+            this.cmbRetry.Location = new System.Drawing.Point(94, 51);
+            this.cmbRetry.Name = "cmbRetry";
+            this.cmbRetry.Size = new System.Drawing.Size(155, 21);
+            this.cmbRetry.TabIndex = 4;
+            // 
+            // dgvStatus
+            // 
+            this.dgvStatus.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStatus.Location = new System.Drawing.Point(12, 101);
+            this.dgvStatus.Name = "dgvStatus";
+            this.dgvStatus.Size = new System.Drawing.Size(435, 278);
+            this.dgvStatus.TabIndex = 5;
+            // 
+            // chkPlaySound
+            // 
+            this.chkPlaySound.AutoSize = true;
+            this.chkPlaySound.Location = new System.Drawing.Point(12, 78);
+            this.chkPlaySound.Name = "chkPlaySound";
+            this.chkPlaySound.Size = new System.Drawing.Size(201, 17);
+            this.chkPlaySound.TabIndex = 6;
+            this.chkPlaySound.Text = "&Play a sound on successful response";
+            this.chkPlaySound.UseVisualStyleBackColor = true;
+            // 
+            // btnExpand
+            // 
+            this.btnExpand.Location = new System.Drawing.Point(372, 385);
+            this.btnExpand.Name = "btnExpand";
+            this.btnExpand.Size = new System.Drawing.Size(75, 23);
+            this.btnExpand.TabIndex = 7;
+            this.btnExpand.Text = "&Show Less";
+            this.btnExpand.UseVisualStyleBackColor = true;
+            this.btnExpand.Click += new System.EventHandler(this.btnExpand_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(372, 25);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 70);
+            this.btnGo.TabIndex = 8;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // chkUseProxy
+            // 
+            this.chkUseProxy.AutoSize = true;
+            this.chkUseProxy.Checked = true;
+            this.chkUseProxy.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkUseProxy.Location = new System.Drawing.Point(219, 78);
+            this.chkUseProxy.Name = "chkUseProxy";
+            this.chkUseProxy.Size = new System.Drawing.Size(147, 17);
+            this.chkUseProxy.TabIndex = 9;
+            this.chkUseProxy.Text = "&Use system proxy settings";
+            this.chkUseProxy.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 441);
+            this.Controls.Add(this.chkUseProxy);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnExpand);
+            this.Controls.Add(this.chkPlaySound);
+            this.Controls.Add(this.dgvStatus);
+            this.Controls.Add(this.cmbRetry);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUri);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "GetDown";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStatus)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tslMostRecentStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUri;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbRetry;
+        private System.Windows.Forms.DataGridView dgvStatus;
+        private System.Windows.Forms.CheckBox chkPlaySound;
+        private System.Windows.Forms.Button btnExpand;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.CheckBox chkUseProxy;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ GetDown/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Media;
+
+namespace GetDown
+{
+    public partial class Form1 : Form
+    {
+        BindingList<QueryLogEntry> QueryLog = new BindingList<QueryLogEntry>();
+        BackgroundWorker QueryWorker = new BackgroundWorker() { WorkerReportsProgress = true, WorkerSupportsCancellation = true };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Data-bind options box
+            var RetryOptions = new Dictionary<string, int>
+            {
+                {"1", 1},
+                {"5", 5},
+                {"10", 10},
+                {"50", 50 },
+                {"100", 100 },
+                {"Until response received",-1 }
+            };
+            cmbRetry.DataSource = new BindingSource(RetryOptions, null);
+            cmbRetry.DisplayMember = "Key";
+            cmbRetry.ValueMember = "Value";
+            cmbRetry.SelectedIndex = 5;
+
+            // Data-bind 
+            dgvStatus.DataSource = QueryLog;
+            dgvStatus.Columns["MessageColour"].Visible = false;
+            dgvStatus.DataMemberChanged += DgvStatus_DataMemberChanged;
+
+        }
+
+        private void DgvStatus_DataMemberChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            // Are we already running?
+            if (QueryWorker.IsBusy)
+            {
+                QueryWorker.CancelAsync();
+            }
+            else
+            {
+                // Check URI
+                if (Uri.IsWellFormedUriString(txtUri.Text, UriKind.Absolute))
+                {
+                    string Uri = txtUri.Text;
+                    int RetryLimit = 0;
+
+                    // Test combo box - user can input anytrhing so make sure it's an int or our preferred "until response received" string.
+                    if (cmbRetry.Text == "Until response received")
+                    {
+                        // Go forever or until it's up
+                        RetryLimit = -1;
+                    }
+                    else if (int.TryParse(cmbRetry.Text, out RetryLimit) && RetryLimit > 0)
+                    {
+                        // We're good to go and we've set RetryLimit.
+                    }
+                    else
+                    {
+                        MessageBox.Show("\"" + cmbRetry.Text + "\" is not a valid number of times to retry.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                    // Either RetryLimit is -1 (hence, set by us) or it's > 0 (hence, set by user). Go-time.
+                    if (RetryLimit != 0)
+                    {
+                        // Let's make a background process do this so our UI is still responsive.
+                        QueryWorker.DoWork += QueryLoopJob;
+                        QueryWorker.ProgressChanged += QueryWorker_ProgressChanged;
+                        QueryWorker.RunWorkerCompleted += QueryWorker_RunWorkerCompleted;
+                        QueryWorker.RunWorkerAsync(argument: new Tuple<string, int>(Uri, RetryLimit));
+                        btnGo.Text = "Stop";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("\"" + txtUri.Text + "\" is not a valid URI.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void QueryLoopJob(object sender, DoWorkEventArgs e)
+        {
+            var WorkerProcess = ((BackgroundWorker)sender);
+            using (var Client = new WebClient())
+            {
+                if (chkUseProxy.Checked)
+                {
+                    Client.Proxy = WebRequest.GetSystemWebProxy();
+                    Client.Proxy.Credentials = System.Net.CredentialCache.DefaultCredentials;
+                }
+
+                string Uri = ((Tuple<string, int>)e.Argument).Item1;
+                string Response = null;
+                int LoopCount = 0;
+                int MaxLoop = ((Tuple<string, int>)e.Argument).Item2;
+
+                do
+                {
+                    // Make sure we're supposed to be doing this.
+                    if (WorkerProcess.CancellationPending == false)
+                    {
+                        LoopCount++;
+                        var LogEntry = new QueryLogEntry(Uri, "Querying...");
+                        WorkerProcess.ReportProgress(LoopCount, LogEntry);
+                        try
+                        {
+                            Response = Client.DownloadString(Uri);
+                            LogEntry.Message = "Success";
+                            LogEntry.MessageColour = Color.DarkGreen;
+                            WorkerProcess.ReportProgress(LoopCount, LogEntry);
+                            if (chkPlaySound.Checked) { SystemSounds.Beep.Play(); }
+                            // Manually set our exit condition to stop looping.
+                            LoopCount = MaxLoop;
+                            WorkerProcess.CancelAsync();
+                        }
+                        catch (WebException wex)
+                        {
+                            LogEntry.Message = wex.Message;
+                            WorkerProcess.ReportProgress(LoopCount, LogEntry);
+                            Wait(60);
+                        }
+                    }
+                    else
+                    {
+                        // Quick & dirty exit.
+                        LoopCount = MaxLoop;
+                    }
+                } while (LoopCount != MaxLoop);
+
+
+            }
+        }
+
+
+        private void Wait(int seconds)
+        {
+            var Timer1 = new Timer();
+            if (seconds == 0 || seconds < 0) { return; }
+            Timer1.Interval = 1000;
+            Timer1.Enabled = true;
+            Timer1.Start();
+            Timer1.Tick += (s, e) =>
+            {
+                toolStripStatusLabel1.Text = string.Format("Waiting. {0} seconds remaining.", seconds);
+                seconds--;
+                if (seconds == 0)
+                {
+                    toolStripStatusLabel1.Text = "";
+                    Timer1.Enabled = false;
+                    Timer1.Stop();
+                }
+            };
+            while (Timer1.Enabled)
+            {
+                // Stop timer if user clicks "stop".
+                if (QueryWorker.CancellationPending)
+                {
+                    toolStripStatusLabel1.Text = "";
+                    Timer1.Enabled = false;
+                    Timer1.Stop();
+                }
+                Application.DoEvents();
+            }
+        }
+
+        private void QueryWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            btnGo.Text = "Go";
+        }
+
+        private void QueryWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            QueryLogEntry LogEntry = e.UserState as QueryLogEntry;
+            if (QueryLog.Count > 0 && QueryLog[QueryLog.Count - 1].QueryDate == LogEntry.QueryDate)
+            {
+                QueryLog[QueryLog.Count - 1].Message = LogEntry.Message;
+                QueryLog[QueryLog.Count - 1].MessageColour = LogEntry.MessageColour;
+            }
+            else
+            {
+                QueryLog.Add(LogEntry);
+            }
+        }
+
+
+        class QueryLogEntry
+        {
+            public DateTime QueryDate { get; set; }
+            public string URI { get; set; }
+            public string Message { get; set; }
+            public Color? MessageColour { get; set; }
+
+            public QueryLogEntry(string uri, string message, Color? colour = null)
+            {
+                QueryDate = DateTime.Now;
+                URI = uri;
+                Message = message;
+                MessageColour = colour ?? SystemColors.ControlText;
+            }
+        }
+
+        private void btnExpand_Click(object sender, EventArgs e)
+        {
+            Button Btn = sender as Button;
+            // Expanded?
+            if (Btn.Top > 200)
+            {
+                dgvStatus.Visible = false;
+                Btn.Top = 101;
+                Btn.Text = "&Show More";
+                Btn.FindForm().Height = 193;
+            }
+            else
+            {
+                dgvStatus.Visible = true;
+                Btn.Top = 385;
+                Btn.Text = "&Show Less";
+                Btn.FindForm().Height = 480;
+            }
+        }
+    }
+}
